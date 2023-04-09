05b2090c6155c56ae29dd97f66210fe6e5eac37f
NaqiNick <naqi.nick@gmail.com>
2023-04-10 04:29:02 +0500

    feat: added validation for null

--- rewaa_ingestion_api/serializers.py
+++ rewaa_ingestion_api/serializers.py
@@ -15,8 +15,10 @@ class SensorPayloadSerializer(serializers.Serializer):
     def get_value(self, value):
         return self.initial_data.get('value')
 
-    def validate_dublicate_value(self, sensor_id, value):
+    def validate_data(self, sensor_id, value):
         previous_value = cache.get(sensor_id)
+        if not value:
+            raise serializers.ValidationError({"value": "A value is required."})
         if value == previous_value:
             raise serializers.ValidationError({"value": "The current value from the sensor is the same as previous "
                                                         "value."})
@@ -27,6 +29,6 @@ class SensorPayloadSerializer(serializers.Serializer):
     def save(self, **kwargs):
         sensor_id = self.validated_data.get('sensor_id')
         value = self.initial_data.get('value')
-        self.validate_dublicate_value(sensor_id=sensor_id, value=value)
+        self.validate_data(sensor_id=sensor_id, value=value)
         insert_data_async.delay(self.collection_name, self.data)
         self.cache_current_value(sensor_id=sensor_id, value=value)
